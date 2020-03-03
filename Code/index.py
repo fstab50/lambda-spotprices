7d6ce233b823c9b4ad6aa3fd4435d594b964cc6f
Blake Huber <blakeca00@gmail.com>
2020-03-03 11:36:43 -0600

    rm

--- Code/index.py
+++
@@ -1,9 +0,0 @@
-"""Summary:
-
- Module pyalias container pointer
-
-    - this module allows all code to be enclosed in
-      the module "core" in a flat lambda namespace
-"""
-
-from core.ec2prices import lambda_handler
